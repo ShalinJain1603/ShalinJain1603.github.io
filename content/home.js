b3d745ad792244f30c8c4037f9029f5238ac25b7
ShalinJain1603 <shalin@iitg.ac.in>
2022-06-24 17:41:53 +0530

    Updated the wrong year

--- content/home.js
+++ content/home.js
@@ -7,7 +7,7 @@ I am tech enthusiast and have explored various fields like Data Analysis, Web De
 I have an urge to experiment with different things and learn something new everyday. I live in Udaipur
 Rajasthan currently and if you want to contact me than mail me at <b>shalinjain349@gmail.com</b>.`;
 
-education.innerHTML = `I have completed my B.Tech in Chemical Engineering from <b>IIT Guwahati</b> in 2018. 
+education.innerHTML = `I have completed my B.Tech in Chemical Engineering from <b>IIT Guwahati</b> in 2022. 
 I have also done several courses in Web development and Data Science. I have passed my senior secondary education
  from CBSE with <b>94.2%</b> from MDS School,Udaipur and secondary education
  from CBSE with <b>10 CGPA</b> from St. Paul's, Udaipur.`;
