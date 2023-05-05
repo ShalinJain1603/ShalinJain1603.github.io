88161468b350b1e626e7fb1051c609290291d314
ShalinJain1603 <shalin@iitg.ac.in>
2023-05-06 01:37:04 +0530

    edited on 6th may 2023

--- content/home.js
+++ content/home.js
@@ -2,7 +2,7 @@ let introduction = document.querySelector("#introduction");
 let education = document.querySelector("#education");
 let job = document.querySelector("#job");
 
-introduction.innerHTML = `Hello !! Myself Shalin Jain, I am a 22 year old B. Tech Graduate. 
+introduction.innerHTML = `Hello !! Myself Shalin Jain, I am a 23 year old B. Tech Graduate. 
 I am tech enthusiast and have explored various fields like Data Analysis, Web Development, Machine Learning, etc.
 I have an urge to experiment with different things and learn something new everyday. I live in Udaipur
 Rajasthan currently and if you want to contact me than mail me at <b>shalinjain349@gmail.com</b>.`;
@@ -12,4 +12,4 @@ I have also done several courses in Web development and Data Science. I have pas
  from CBSE with <b>94.2%</b> from MDS School,Udaipur and secondary education
  from CBSE with <b>10 CGPA</b> from St. Paul's, Udaipur.`;
 
-job.innerHTML = `I have recently graduated from college and will start my first job in July 2022 in <b>Google India</b>.`;
+job.innerHTML = `I have been working in <b>Google India</b> banglore since July 2022 as Software Developer.`;
